83223fbca12d573200214919ee9d9b099a4ceeac
boromon <borowa.monika@gmail.com>
2024-10-20 14:07:48 +0200

    Connected forecast to the API

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+
+  getForecast(response.data.city);
 }
 
 function search(event) {
@@ -44,15 +46,15 @@ function formatDate(date) {
   return `${formattedDay} ${hours}:${minutes}`;
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+function getForecast(city) {
+  let apiKey = "496o6a9a0e303dbaa1t34f17418ccfcf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
-let currentDateELement = document.querySelector("#current-date");
-let currentDate = new Date();
+function displayForecast(response) {
+  console.log(response.data);
 
-currentDateELement.innerHTML = formatDate(currentDate);
-
-function displayForecast() {
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
@@ -76,4 +78,16 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHtml;
 }
 
-displayForecast();
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let defaultCity = "Berlin";
+let apiKey = "b2a5adcct04b33178913oc335f405433";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${defaultCity}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
